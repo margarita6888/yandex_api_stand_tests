9025fb136b35194ab15d411085a9f203e8159db3
Kuvaeva Margarita <mvkuvaeva@yandex.ru>
2023-08-20 19:13:45 +0300

    Добавила комментарий

--- create_user_test.py
+++ create_user_test.py
@@ -1,7 +1,7 @@
 import sender_stand_request
 import data
 
-
+# новый комментарий#
 def get_user_body(first_name):
     current_body = data.user_body.copy()
     current_body["firstName"] = first_name
